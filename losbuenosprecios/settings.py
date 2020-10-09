075f1434a0963d56f85af8edc13fdcaf3e22562c
DanielGuarchaj <gtdanniel@gmail.com>
2020-10-08 23:51:38 -0600

    Add s3 configs

--- losbuenosprecios/settings.py
+++ losbuenosprecios/settings.py
@@ -149,5 +149,8 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+AWS_S3_REGION_NAME = 'us-east-2' #change to your region
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
